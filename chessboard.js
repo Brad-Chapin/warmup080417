bd9f93297e19f86ab85a6da609ec8d04bc7b2816
Brad Chapin <paledoor@yahoo.com>
2017-08-04 13:01:56 -0700

    chessboard function written and added

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,16 @@
 function chessboard (size) {
-  for (let i = 0; i < size; i++) {}
+  let white = " ", black = "#";
+  for (let i = 0; i < size; i++) {
+    let row = "";
+    for (let j = 0; j < size; j++) {
+      if ((i+j)%2 == 0) {
+        row += white;
+      } else {
+        row += black;
+      }
+    }
+    console.log(row);
+  }
 };
+
+chessboard (8);
